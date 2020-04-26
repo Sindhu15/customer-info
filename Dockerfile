3323b072ca22e2c9bd267f6a5ad9202517366bda
Sindhu <sindhu.a@knolskape.com>
2020-04-26 23:15:37 +0530

    readme

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,15 @@
-# FROM postgres 
-# ENV POSTGRES_PASSWORD postgres 
-# ENV POSTGRES_DB customer_info 
-
-
 FROM node:10
-# USER root:node
-
-RUN npm install -g nodemon
 WORKDIR /usr/src/app
-# 
 
 COPY . .
 
 RUN yarn --cwd ./api
 RUN yarn --cwd ./ui
-# RUN yarn --cwd ./ui start
 
-# EXPOSE 3000
 EXPOSE 3000
 EXPOSE 3010
-# ENTRYPOINT ["/bin/sh","-c"]
 
-CMD [ "nodemon" , "./api/app.js"]
 
-# CMD ["yarn" , "./ui" , "start" ]
+CMD [ "node" , "./api/app.js"]
 
 
--- start-app.sh
+++ start-app.sh
@@ -1,30 +1,4 @@
-# docker ps -a | awk '{ print $1,$2 }' | grep sindhu/customer_info_local | awk '{print $1 }' | xargs -I {} docker rm --force {}
-# docker ps -a | awk '{ print $1,$2 }' | grep customer-postgres-docker | awk '{print $1 }' | xargs -I {} docker rm --force {}
-
-# docker rmi sindhu/customer_info_local
-# docker rm customer-postgres-docker --force
-
-# docker build -t sindhu/customer_info_local .
-# docker run -p 3001:3000 -d sindhu/customer_info_local
-
-# docker run --name customer-postgres-docker -e POSTGRES_PASSWORD=postgres -p 5440:5432 -d postgres
-
-# docker exec -it customer-postgres-docker psql -U postgres -c "CREATE DATABASE customer;"
-
-# . start-app.sh
-
 docker-compose down
 docker-compose build
 docker-compose up -d
-docker exec -it customer_info_app bash
-
-# exec api/node_modules/.bin/sequelize db:migrate
-# node_modules/.bin/sequelize db:migrate
-
-
-# node_modules/.bin/sequelize init
-# cd api/sequelize init
-# sequelize init:config                       
-# sequelize init:migrations                   
-# sequelize init:models                       
-# sequelize init:seeders 
+docker exec -it customer_info_app bash -c "cd api; sleep 5; sh run-migrations-seeders.sh; cd ../ui; yarn start;"
